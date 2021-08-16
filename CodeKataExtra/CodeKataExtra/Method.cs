712f8aeabb21ef21761b6963dd08b6420565dfe6
Alex Rae <arae@spartaglobal.com>
2021-08-16 16:07:16 +0100

    fixed code + added correct test cases

--- CodeKataExtra/CodeKataExtra/Method.cs
+++ CodeKataExtra/CodeKataExtra/Method.cs
@@ -8,30 +8,37 @@ namespace CodeKataMethods
     {
         //Create a method which takes a string and returns an array of duplicate characters
         //e.g. "Nishant Mandal" => (a,n)
+        public static void Main()
+        {
+
+        }
 
         public static char[] ReturnDuplicate(string input)
         {
-            input = input.ToLower();
+            string inputLower = input.ToLower();
 
-            char[] outputArray = input.ToCharArray();
+            char[] outputArray = inputLower.ToCharArray();
 
-            string result = "";
+            string duplicateChar = "";
 
-            string duplicate = "";
+            string seenChar = "";
 
             for (int i = 0; i < outputArray.Length; i++)
             {
-                if (duplicate.Contains(outputArray[i].ToString()))
+                if (seenChar.Contains(outputArray[i].ToString()))
                 {
-                    result += outputArray[i].ToString();
+                    if (!duplicateChar.Contains(outputArray[i].ToString()))
+                    {
+                        duplicateChar += outputArray[i].ToString();
+                    }
                 }
                 else
                 {
-                    duplicate += outputArray[i].ToString();
+                    seenChar += outputArray[i].ToString();
                 }
             }
 
-            char[] resultArray = result.ToCharArray();
+            char[] resultArray = duplicateChar.ToCharArray();
 
             return resultArray;
         }
--- CodeKataExtra/UnitTest/UnitTest1.cs
+++ CodeKataExtra/UnitTest/UnitTest1.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using CodeKataMethods;
 
 namespace CodeKataTests
 {
@@ -10,11 +11,20 @@ namespace CodeKataTests
         }
 
         [Test]
-        [TestCase("Nish Mandal", "nana")]
-		public void GivenAString_Return_Correct_ArrayOfDuplicatesCharacters(string input, char[] expResult)
+        [TestCase("Nishant Mandal", new char[] { 'n', 'a' })]
+        [TestCase("Martin Beard", new char[] { 'a', 'r' })]
+        [TestCase("Alex Rae", new char[] { 'a', 'e' })]
+        public void GivenAString_Return_Correct_ArrayOfDuplicateCharacters(string input, char[] expResult)
 		{
-            var charArray = input.ToCharArray();
+            var charArray = Method.ReturnDuplicate(input);
 			Assert.That(charArray, Is.EqualTo(expResult));
 		}
+
+        [TestCase("Alex")]
+        public void GivenAStringWithNoDuplicates_Return_EmptyArray(string input)
+        {
+            var charArray = Method.ReturnDuplicate(input);
+            Assert.That(charArray.Length, Is.EqualTo(0));
+        }
     }
 }
