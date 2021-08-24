3ab0cc8696202a585964b90f1003b22d272b4ec9
Alex Rae <arae@spartaglobal.com>
2021-08-24 12:34:32 +0100

    completed fibonacci code and tests

--- CodeKataExtra/CodeKataExtra/Method.cs
+++ CodeKataExtra/CodeKataExtra/Method.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 
+
 namespace CodeKataMethods
 {
     public class Method
     {
-        //Create a method which takes a string and returns an array of duplicate characters
-        //e.g. "Nishant Mandal" => (a,n)
         public static void Main()
         {
+            //Create a method which takes a string and returns an array of duplicate characters
+            //e.g. "Nishant Mandal" => (a,n)
 
+            //Create a method which returns the sum of fibonacci numbers up to the nth fibonacci number.
         }
 
         public static char[] ReturnDuplicate(string input)
@@ -41,7 +43,23 @@ namespace CodeKataMethods
 
             return resultArray;
         }
+
+        public static int FibonacciNumbers(int input)
+        {
+            List<int> fSequence = new List<int>() { 0, 1 };
+            
+            if (input <= 0 || input > 44)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else
+            {
+                while ((int)fSequence.Count <= input + 2)
+                {
+                    fSequence.Add(fSequence[fSequence.Count - 1] + fSequence[fSequence.Count - 2]);
+                }
+                return (fSequence[fSequence.Count - 1] - 1);
+            }
+        }
     }
-
-
 }
--- CodeKataExtra/UnitTest/UnitTest1.cs
+++ CodeKataExtra/UnitTest/UnitTest1.cs
@@ -1,30 +1,64 @@
 using NUnit.Framework;
 using CodeKataMethods;
+using System;
 
 namespace CodeKataTests
 {
-    public class Tests
+    public class DuplicateTests
     {
         [SetUp]
         public void Setup()
         {
         }
 
-        [Test]
         [TestCase("Nishant Mandal", new char[] { 'n', 'a' })]
         [TestCase("Martin Beard", new char[] { 'a', 'r' })]
         [TestCase("Alex Rae", new char[] { 'a', 'e' })]
         public void GivenAString_Return_Correct_ArrayOfDuplicateCharacters(string input, char[] expChar)
-		{
+        {
             var charArray = Method.ReturnDuplicate(input);
-			Assert.That(charArray, Is.EqualTo(expChar));
-		}
+            Assert.That(charArray, Is.EqualTo(expChar));
+        }
 
         [TestCase("Alex")]
         public void GivenAStringWithNoDuplicates_Return_EmptyArray(string input)
         {
             var charArray = Method.ReturnDuplicate(input);
             Assert.That(charArray.Length, Is.EqualTo(0));
+        }
+    }
+
+    public class FibonacciTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase(44, 1_836_311_902)]
+        [TestCase(27, 514228)]
+        [TestCase(26, 317810)]
+        [TestCase(13, 609)]
+        [TestCase(6, 20)]
+        [TestCase(1, 1)]
+        public void GivenAnInputOfN_Return_CorrectSumOfFibonacciNumbersUpToN(int input, int expTotal)
+        {
+            int sequence = Method.FibonacciNumbers(input);
+            Assert.That(sequence, Is.EqualTo(expTotal));
+        }
+
+        [Test]
+        public void GivenANegativeInputOfN_Throw_Exception()
+        {
+            Assert.That(() => Method.FibonacciNumbers(-1), 
+                Throws.TypeOf<ArgumentOutOfRangeException>().With.Message.Contains("Specified argument was out of the range of valid values."));
+        }
+
+        [Test]
+        public void GivenAnInputOfNResultingInOverflow_Throw_Exception()
+        {
+            Assert.That(() => Method.FibonacciNumbers(45), 
+                Throws.TypeOf<ArgumentOutOfRangeException>().With.Message.Contains("Specified argument was out of the range of valid values."));
         }
     }
 }
