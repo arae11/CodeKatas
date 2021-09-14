0babfcced5c05694edb918b873460a34b362c24d
Alex Rae <arae@spartaglobal.com>
2021-09-14 21:48:23 +0100

    completed extra code katas + unit tests

--- CodeKataExtra/CodeKataExtra/Method.cs
+++ CodeKataExtra/CodeKataExtra/Method.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
-
+using System.Text;
 
 namespace CodeKataMethods
 {
@@ -9,20 +10,78 @@ namespace CodeKataMethods
     {
         public static void Main()
         {
-            //Create a method which takes a string and returns an array of duplicate characters
-            //e.g. "Nishant Mandal" => (a,n)
+            //Console.WriteLine("The quick brown fox jumps over the lazy dog");
+            #region AddArrayOfNumsThatAreDivisibleBy2Or3
+            int[] nums = new int[] { 21, 45, 26, 54, 11, 76, 44, 88, 23 };
+            Console.WriteLine(ArrayOfNums(nums));
+            #endregion
 
-            //Create a method which returns the sum of fibonacci numbers up to the nth fibonacci number.
+            #region ReturnString
+            //int[] array = new int[] { 1, 2, 3, 4 };
+            //Console.WriteLine(ReturnString(array));
+            #endregion
+
+            #region CharacterIdentifier
+            //while (true)
+            //{
+            //Console.WriteLine($"Character Identifier\nPlease enter a character");
+            //var input = Console.ReadLine();
+            //var type = CharacterIdentifier(input);
+            //Console.WriteLine($"Entered character {input} is a {type}");
+            //Console.WriteLine("Do you want to exit? (# for yes)");
+            //var exit = Console.ReadLine();
+            //if (exit == "#")
+            //{
+            //    Console.WriteLine("Goodbye");
+            //    break;
+            //}
+            //}
+            #endregion
+
+            #region AverageMarks
+            //Console.WriteLine("AverageMarks");
+            //var inputMarks = Console.ReadLine();
+            //Console.WriteLine($"Average marks of ({inputMarks}) = {AverageMarks(inputMarks)}");
+            #endregion
+
+            #region ConvertTemp
+            //Console.WriteLine("Would you like to convert to Fahrenheit or Celsius?");
+            //string conversion = Console.ReadLine();
+            //Console.WriteLine("Please enter the temperature for conversion.");
+            //string temp = Console.ReadLine();
+            //var newTemp = ConvertTemperature(conversion, float.Parse(temp));
+            //Console.WriteLine($"Your new temperature is: {newTemp}");
+            #endregion
+
+            #region Recursion
+            //Console.Write("Enter a string: ");
+            //var inputString = Console.ReadLine(); //read input from user
+
+            //var reverseString = ReverseStringRecursion(inputString); // invoke the static method
+            //Console.WriteLine($"Reverse of a String: {reverseString}"); // print the result
+            #endregion
+
+            #region DifferenceBetweenTwoElementsInAnArray
+            //int[] numArray = { 2, 3, 1, 7, 9, 5, 11, 3, 5 };
+            //Console.WriteLine($"Original array: {String.Join(", ", numArray)}");
+
+            //Console.WriteLine($"The maximum difference between two elements of the given array is : " +
+            //    $"{DiffBetweenTwoElements(numArray)}");
+            #endregion
+
+            #region SecondHighest
+            //int[] numArray = new int[] { 2, 8, 1, 4, 2, 6, 7, 3, 4, 1 };
+            //Console.WriteLine(SecondHighest(numArray));
+            #endregion
         }
 
+        #region TakeStringAndReturnArrayOfDuplicateCharacters
+        //Create a method which takes a string and returns an array of duplicate characters.
         public static char[] ReturnDuplicate(string input)
         {
             string inputLower = input.ToLower();
-
             char[] inputArray = inputLower.ToCharArray();
-
             string duplicateChar = "";
-
             string seenChar = "";
 
             for (int i = 0; i < inputArray.Length; i++)
@@ -43,23 +102,303 @@ namespace CodeKataMethods
 
             return resultArray;
         }
+        #endregion
 
+        #region ReturnSumOfFibonacciNumbers
+        //Create a method which returns the sum of fibonacci numbers up to the nth fibonacci number.
         public static int FibonacciNumbers(int input)
         {
             List<int> fSequence = new List<int>() { 0, 1 };
-            
+            int firstNumber = 0; 
+            int secondNumber = 1; 
+            int sum = 0; 
+
             if (input <= 0 || input > 44)
             {
                 throw new ArgumentOutOfRangeException();
             }
             else
             {
-                while ((int)fSequence.Count <= input + 2)
+                for (int i = 0; i <= input; i++)
                 {
-                    fSequence.Add(fSequence[fSequence.Count - 1] + fSequence[fSequence.Count - 2]);
+                    sum = firstNumber + secondNumber;
+                    firstNumber = secondNumber;
+                    secondNumber = sum;
                 }
-                return (fSequence[fSequence.Count - 1] - 1);
+                return sum - 1;
             }
         }
+        #endregion
+
+        #region ReverseAStringUsingRecursion
+        //Create a program to reverse a String using recursion. 
+        public static string ReverseStringRecursion(string input)
+        {
+            if (input.Length <= 1)
+                return input;
+            else
+            {
+                return ReverseStringRecursion(input.Substring(1)) + input[0];
+            }
+        }
+
+        public static string ReverseString(string input)
+        {
+            List<char> inputList = input.ToList();
+            StringBuilder outputString = new StringBuilder();
+
+            for (int i = inputList.Count - 1; i >= 0; i--)
+            {
+                outputString.Append(inputList[i]);
+            }
+            return outputString.ToString();
+        }
+
+        public static char[] ReverseStringStack(string input)
+        {
+            char[] outputArray = new char[input.Length];
+            var stack = new Stack<char>();
+
+            foreach (var x in input)
+            {
+                stack.Push(x);
+            }
+            for (int i = 0; i < outputArray.Length; i++)
+            {
+                outputArray[i] = stack.Pop();
+            }
+            return outputArray;
+        }
+        #endregion
+
+        #region DifferenceBetweenTwoElementsInAnArray
+        //takes an Int Array and returns the MAX-imum difference between the contents of the array
+        public static int DiffBetweenTwoElements(int[] nums)
+        {
+            // Gareth efficient way
+            //Array.Sort<int>(nums);
+            //int diff = nums[nums.Length - 1] - nums[0];
+
+            int max = int.MinValue;
+            int min = int.MaxValue;
+
+            if (nums == null || nums.Length < 2)
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                foreach (var x in nums)
+                {
+                    if (x > max)
+                    {
+                        max = x;
+                    }
+                    if (x < min)
+                    {
+                        min = x;
+                    }
+                }
+                return max - min;
+            }
+        }
+        #endregion
+
+        #region ConvertTemperature
+        public static float ConvertTemperature(string conversion, float temp)
+        {
+            string unit = conversion.ToLower();
+            float result = 0;
+
+            if (unit == "fahrenheit")
+            {
+                result = ((temp - 32f) * (5f / 9f));
+            }
+            else if (unit == "celsius")
+            {
+                result = ((temp * (9f / 5f) + 32f));
+            }
+            return result;
+        }
+        #endregion
+
+        #region AverageMarks
+        //Create a program that will behave as follows from the command line:
+        //c:/>AverageMarks 2 100 50
+        //c:/>Average marks of(2,100,50) = 50.666666666666664
+        public static float AverageMarks(string inputMarks)
+        {
+            if (inputMarks == null)
+            {
+                throw new ArgumentException();
+            }
+
+            string[] marks = inputMarks.Split(" ");
+            float sum = 0;
+             
+            foreach (string s in marks)
+            {
+                if (float.Parse(s) < 0)
+                {
+                    throw new ArgumentException();
+                }
+                sum += float.Parse(s);
+            }
+            return sum / marks.Length;
+        }
+
+        #endregion
+
+        #region CharacterIdentifier
+        //Create a program that will behave as follows from the command line:
+
+        //c:/>CharIdentifier
+        //c:/>a
+        //c:/>Entered character a is  Vowel
+        //c:/>L
+        //c:/>Entered character L is  Consonant
+        //c:/>#
+        //c:/>Goodbye
+
+        public static string CharacterIdentifier(string input)
+        {
+            List<char> vowel = new List<char>() { 'A', 'E', 'I', 'O', 'U' };
+            List<char> consonant = new List<char>() { 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V','W','X','Y','Z' };
+
+            string letter = input.ToUpper();
+            string output = "";
+
+            for (int i = 0; i < vowel.Count(); i++)
+            {
+                if (vowel[i].ToString() == letter)
+                {
+                    output = "vowel";
+                }
+            }
+            for (int i = 0; i < consonant.Count(); i++)
+            {
+                if (consonant[i].ToString() == letter)
+                {
+                    output = "consonant";
+                }
+            }
+            if (output == "")
+            {
+                output = "Invalid character";
+            }
+            return output;
+
+            /*
+             * if (vowel.Contains(letter)) return "Vowel";
+             * else if (consonant.Contains(letter)) return "Consonant";
+             * return null;
+             */
+        }
+        #endregion
+
+        #region SecondHighestNumberInArray
+        public static int SecondHighest(int[] numArray)
+        {
+            Array.Sort(numArray);
+            return numArray[numArray.Length - 1];
+        }
+        #endregion
+
+        #region StringWithVowelsRemoved
+        //return a string with the vowels removed to upper
+        public static string NoVowels(string input)
+        {
+            char[] vowels = new char[] { 'A', 'E', 'I', 'O', 'U' };
+            string output = "";
+            string uInput = input.ToUpper();
+            char[] inputArray = uInput.ToArray();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!vowels.Contains(inputArray[i]))
+                {
+                   output += inputArray[i];
+                }
+            }
+            return output;
+        }
+        #endregion
+
+        #region ReturnStringFromArray
+        // Return a string from an array
+        public static string ReturnString (int[] input)
+        {
+            string output = string.Empty;
+            foreach (var num in input)
+            {
+                output += num;
+            }
+            return output;
+        }
+        #endregion
+
+        #region AddArrayOfNumsThatAreDivisibleBy2Or3
+        public static int ArrayOfNums(int[] nums) 
+        {
+            int result = 0;
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    if (nums[i] % 2 == 0 || nums[i] % 3 == 0)
+            //    {
+            //        result += i;
+            //    }
+            //}
+            //return result;
+
+            foreach(var num in nums)
+            {
+                if (num % 2 == 0 || num % 3 == 0)
+                {
+                    result += num;
+                }
+            }
+            return result;
+        }
+        #endregion
+
+        #region AnimalYears
+        public static int[] humanYearsCatYearsDogYears(int humanYears)
+        {
+
+            int catYears = 24;
+            int dogYears = 24;
+
+            if (humanYears < 1)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else
+            {
+                for (int i = 0; i <= humanYears; i++)
+                {
+                    if (humanYears == 1)
+                    {
+                        catYears = 15;
+                        dogYears = 15;
+                    }
+                    else if (humanYears == 2)
+                    {
+                        catYears = 24;
+                        dogYears = 24;
+                    }
+                    else
+                    {
+                        for (i = 3; i <= humanYears; i++)
+                        {
+                            catYears += 4;
+                            dogYears += 5;
+                        }
+                    }
+                }
+            }
+            return new int[] { humanYears, catYears, dogYears };
+        }
+        #endregion
     }
+
 }
--- CodeKataExtra/UnitTest/UnitTest1.cs
+++ CodeKataExtra/UnitTest/UnitTest1.cs
@@ -61,4 +61,130 @@ namespace CodeKataTests
                 Throws.TypeOf<ArgumentOutOfRangeException>().With.Message.Contains("Specified argument was out of the range of valid values."));
         }
     }
+
+    public class ReverseStringTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase("This is my string", "gnirts ym si sihT")]
+        [TestCase("Hello world!", "!dlrow olleH")]
+        [TestCase("The quick brown fox jumps over the lazy dog", "god yzal eht revo spmuj xof nworb kciuq ehT")]
+        public void GivenAnInputOfString_Return_ReversedString(string input, string expResult)
+        {
+            
+            string reversed = Method.ReverseString(input);
+            Assert.That(reversed, Is.EqualTo(expResult));
+        }
+
+        [TestCase("This is my string")]
+        public void GivenAString_Return_ReversedString(string input)
+        {
+            char[] inputArray = input.ToCharArray();
+            Array.Reverse(inputArray);
+
+            Assert.That(Method.ReverseStringStack(input), Is.EqualTo(new string(inputArray)));
+        }
+    }
+
+    public class TwoElementsTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase(new int[] { 2, 3, 1, 7, 9, 5, 11, 3, 5 }, 10)]
+        [TestCase(new int[] { 2, 5, 2, 1, 9, 5, 23, 3, 5 }, 22)]
+        public void GivenAnArrayOfIntegers_Return_CorrectMaximumDifference(int[] nums, int expResult)
+        {
+            Assert.That(() => Method.DiffBetweenTwoElements(nums), Is.EqualTo(expResult));
+        }
+
+        [Test]
+        public void GivenAnArrayWithNullElements_Throw_Exception()
+        {
+            Assert.That(() => Method.DiffBetweenTwoElements(null),Throws.TypeOf<ArgumentException>());
+        }
+
+        [TestCase(new int[] { 2 })]
+        public void GivenAnArrayWithLessThanTwoElements_Throw_Exception(int[] nums)
+        {
+            Assert.That(() => Method.DiffBetweenTwoElements(nums),Throws.TypeOf<ArgumentException>());
+        }
+    }
+
+    public class TemperatureTests
+    {
+        [TestCase("celsius", 0f, 32f)]
+        [TestCase("fahrenheit", 32f, 0f)]
+        [TestCase("celsius", -69f, -92.2f)]
+        [TestCase("fahrenheit", -100f, -73.3f)]
+        public void GivenASelectedUnitAndInputOfTemperature_Return_CorrectConversion (string unit, float input, float expResult)
+        {
+            Assert.That(() => Method.ConvertTemperature(unit, input), Is.EqualTo(expResult).Within(0.1));
+        }
+    }
+
+    public class AverageMarksTests
+    {
+        [TestCase("20 100 50", 56.67f)]
+        [TestCase("30 70 90", 63.33f)]
+        [TestCase("44 62 21 51 11", 37.80f)]
+        public void GivenAStringOfNumbers_Return_CorrectAverageMark(string input, float expResult)
+        {
+            Assert.That(() => Method.AverageMarks(input), Is.EqualTo(expResult).Within(0.01));
+        }
+
+        [Test]
+        public void GivenAStringOfNull_Throw_ArgumentException()
+        {
+            Assert.That(() => Method.AverageMarks(null), Throws.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void GivenAStringContainingNegativeNumbers_Throw_ArgumentException()
+        {
+            Assert.That(() => Method.AverageMarks("-1"), Throws.TypeOf<ArgumentException>());
+        }
+    }
+
+    public class CharacterIdentifierTests
+    {
+        [TestCase("a","vowel")]
+        public void GivenAStringInput_Return_CorrectVowelOrConsonant(string input, string expResult)
+        {
+            Assert.That(() => Method.CharacterIdentifier(input), Is.EqualTo(expResult));
+        }
+    }
+
+    public class ReturnStringTest
+    {
+        [TestCase(new int[] { 1, 2, 3, 4 },"1234")]
+        public void GivenAnInputOfAStringOfIntegers_Return_Integers(int[] input, string expResult)
+        {
+            Assert.That(() => Method.ReturnString(input), Is.EqualTo(expResult));
+        }
+    }
+
+    public class AnimalYearTests
+    {
+        [TestCase(1, new int[] { 1, 15, 15 })]
+        [TestCase(2, new int[] { 2, 24, 24 })]
+        [TestCase(3, new int[] { 3, 28, 29 })]
+        [TestCase(10, new int[] { 10, 56, 64 })]
+        public void GivenAnInputOfHumanYears_Return_CorrectCatAndDogAge(int input, int[] expResult)
+        {
+            Assert.That(() => Method.humanYearsCatYearsDogYears(input), Is.EqualTo(expResult));
+        }
+
+        [Test]
+        public void GivenAnInputLessThanOne_Throw_Exception()
+        {
+            Assert.That(()=> Method.humanYearsCatYearsDogYears(-1), 
+                Throws.TypeOf<ArgumentOutOfRangeException>().With.Message.Contains("Specified argument was out of the range of valid values."));
+        }
+    }
 }
